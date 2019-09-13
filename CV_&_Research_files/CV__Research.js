f0176a2ad452e3e2edab2d2d1b3e87981dc2a008
Florian Ederer <florian.ederer@gmail.com>
2019-09-13 13:57:14 -0400

    Florian Ederer

---
+++ CV_&_Research_files/CV__Research.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.1 local-build-20090422
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('CV_&_Research_files/CV_&_ResearchMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');performPostEffectsFixups()}
---
+++ Contact_files/Contact.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.1 local-build-20090422
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Contact_files/ContactMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');performPostEffectsFixups()}
