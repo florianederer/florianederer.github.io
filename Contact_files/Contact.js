8674b1b71adfb1e64680a691f12d72ea39640953
Florian Ederer <55293121+florianederer@users.noreply.github.com>
2024-09-14 17:14:52 -0400

    update

--- Contact_files/Contact.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.1 local-build-20090422
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Contact_files/ContactMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');performPostEffectsFixups()}
